54929fc401988b290817ac21afad652f885d6a8c
Garvin Ponde <garvinponde@gmail.com>
2024-02-22 16:06:12 -0400

    added form

---
+++ app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    public function create() {
+        return view('feedback-form');
+    }
+    public function send (){
+        
+    }
+}
